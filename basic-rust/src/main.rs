47db1a9204cd32b66691886734c746dbbf83399a
Audyari Wiyono <audy123ari@gmail.com>
2024-11-13 13:42:22 +0700

    2. variabel

--- basic-rust/src/main.rs
+++ basic-rust/src/main.rs
@@ -23,3 +23,58 @@ fn test_main() {
     println!("Audyari Wiyono");
 }
 
+
+//variable
+
+#[test]
+fn variable_test() {
+
+    // Declare a variable `name` and assign it a string
+    let name: &str = "Audyari Wiyono";
+    // Print a greeting message using the `name` variable
+    println!("Hello, {}!", name);
+  
+    // Declare a mutable variable `x` and assign it the value 5
+    let mut x = 5;
+    // Print a message indicating the value of `x`
+    println!("The value of x is: {}", x);
+    // Change the value of `x` to 6
+    x = 6;
+    // Print another message indicating the new value of `x`
+    println!("The value of x is: {}", x);
+
+    // Declare a mutable variable `datanama` and assign it an initial value
+    let mut datanama = "Budi Wiyono";
+    // Print the initial value of `datanama`
+    println!("data nama: {}", datanama);
+    // Change the value of `datanama`
+    datanama = "Audyari Wiyono";
+    // Print the new value of `datanama`
+    println!("data nama updated: {}", datanama);
+
+}
+
+#[test]
+fn variable_test_eror() {
+
+    let x = 5;
+    // Print the value of `x`
+    println!("The value of x is: {}", x);
+    // Attempting to assign a string to `x` will cause a type error
+    //x = "testing";
+    // Print the value of `x` again to demonstrate that it did not change
+    println!("The value of x is: {}", x);
+}
+
+#[test]
+fn shadow_test() {
+
+    // Declare a variable `nama` and assign it a string
+    let nama = "Audyari Wiyono";
+    // Print the initial value of `nama`
+    println!("data nama: {}", nama);
+    // Shadow the `nama` variable with a new value
+    let nama = "Budi Wiyono";
+    // Print the updated value of `nama`
+    println!("data nama updated: {}", nama);
+}
