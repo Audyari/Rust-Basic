3e8691729414dc993f6b7706d4e49d878a227841
Audyari Wiyono <audy123ari@gmail.com>
2024-11-27 22:17:09 +0700

    26. Method

--- basic-rust/target/release/basic-rust.d
+++
@@ -1,1 +0,0 @@
-D:\RUST\Rust-Basic\basic-rust\target\release\basic-rust.exe: D:\RUST\Rust-Basic\basic-rust\src\main.rs
--- basic-rust/target/release/deps/basic_rust-234efd0e320b8f16.d
+++
@@ -1,5 +0,0 @@
-D:\RUST\Rust-Basic\basic-rust\target\release\deps\basic_rust-234efd0e320b8f16.exe: src/main.rs
-
-D:\RUST\Rust-Basic\basic-rust\target\release\deps\basic_rust-234efd0e320b8f16.d: src/main.rs
-
-src/main.rs:
